822889a11e107a8b492dd371a023d671c33b5332
Nobody <nobody@team116.org>
2020-02-17 14:50:36 -0500

    Auto formatted

--- src/main/java/org/firstinspires/ftc/teamcode/record/BlackBox.java
+++ src/main/java/org/firstinspires/ftc/teamcode/record/BlackBox.java
@@ -15,100 +15,105 @@ import java.util.Map;
  * Writes hardware state as a time series and plays it back.
  */
 public class BlackBox {
-  /**
-   * Writes hardware state as a timeseries stream.
-   */
-  public static class Recorder {
     /**
-     * Creates the recorder.
-     * @param hardware The hardware to record.
-     * @param outputStream The output stream to write.
+     * Writes hardware state as a timeseries stream.
      */
-    public Recorder(HardwareMap hardware, OutputStream outputStream) throws Exception {
-      this.hardware = hardware;
-      writer = new TimeseriesStream.Writer(outputStream);
+    public static class Recorder {
+        /**
+         * Creates the recorder.
+         *
+         * @param hardware     The hardware to record.
+         * @param outputStream The output stream to write.
+         */
+        public Recorder(HardwareMap hardware, OutputStream outputStream) throws Exception {
+            this.hardware = hardware;
+            writer = new TimeseriesStream.Writer(outputStream);
+        }
+
+        /**
+         * Records all hardware devices.
+         *
+         * @param time The time to record hardware at (seconds).
+         */
+        public void recordAllDevices(double time) throws Exception {
+            for (Map.Entry<String, DcMotor> entry : hardware.dcMotor.entrySet()) {
+                recordDevice(entry.getKey(), time);
+            }
+            for (Map.Entry<String, Servo> entry : hardware.servo.entrySet()) {
+                recordDevice(entry.getKey(), time);
+            }
+        }
+
+        /**
+         * Records the specific hardware device.
+         *
+         * @param deviceName The device to record.
+         * @param time       The time to record hardware at (seconds).
+         */
+        public void recordDevice(String deviceName, double time) throws Exception {
+            HardwareDevice device = hardware.get(deviceName);
+            double value;
+            if (device instanceof DcMotor) {
+                value = ((DcMotor) device).getPower();
+            } else if (device instanceof Servo) {
+                value = ((Servo) device).getPosition();
+            } else {
+                return;
+            }
+            writer.write(new TimeseriesStream.DataPoint(deviceName, time, value));
+        }
+
+        // The hardware to record.
+        private HardwareMap hardware;
+        // The timeseries stream to write.
+        private TimeseriesStream.Writer writer;
     }
 
     /**
-     * Records all hardware devices.
-     * @param time The time to record hardware at (seconds).
+     * Reads hardware state from a timeseries stream and applies it.
      */
-    public void recordAllDevices(double time) throws Exception {
-      for (Map.Entry<String, DcMotor> entry : hardware.dcMotor.entrySet()) {
-        recordDevice(entry.getKey(), time);
-      }
-      for (Map.Entry<String, Servo> entry : hardware.servo.entrySet()) {
-        recordDevice(entry.getKey(), time);
-      }
-    }
+    public static class Player {
+        /**
+         * Creates the player.
+         *
+         * @param inputStream The input stream to read from.
+         * @param hardware    The hardware to manipulate.
+         */
+        public Player(InputStream inputStream, HardwareMap hardware) throws Exception {
+            this.hardware = hardware;
+            reader = new TimeseriesStream.Reader(inputStream);
+        }
 
-    /**
-     * Records the specific hardware device.
-     * @param deviceName The device to record.
-     * @param time The time to record hardware at (seconds).
-     */
-    public void recordDevice(String deviceName, double time) throws Exception {
-      HardwareDevice device = hardware.get(deviceName);
-      double value;
-      if (device instanceof DcMotor) {
-        value = ((DcMotor)device).getPower();
-      } else if (device instanceof Servo) {
-        value = ((Servo)device).getPosition();
-      } else {
-        return;
-      }
-      writer.write(new TimeseriesStream.DataPoint(deviceName, time, value));
-    }
-
-    // The hardware to record.
-    private HardwareMap hardware;
-    // The timeseries stream to write.
-    private TimeseriesStream.Writer writer;
-  }
-
-  /**
-   * Reads hardware state from a timeseries stream and applies it.
-   */
-  public static class Player {
-    /**
-     * Creates the player.
-     * @param inputStream The input stream to read from.
-     * @param hardware The hardware to manipulate.
-     */
-    public Player(InputStream inputStream, HardwareMap hardware) throws Exception {
-      this.hardware = hardware;
-      reader = new TimeseriesStream.Reader(inputStream);
-    }
-
-    /**
-     * Playbacks the hardware up to the time.
-     * @param time The time to playback up to (seconds).
-     * @return True if playback is still running. False if it has completed.
-     */
-    public boolean playback(double time) throws Exception {
-      List<TimeseriesStream.DataPoint> points = reader.readUntil(time);
-      for (TimeseriesStream.DataPoint p : points) {
-        HardwareDevice device = hardware.get(p.varname);
-        if (device != null) {
-          if (device instanceof DcMotor) {
+        /**
+         * Playbacks the hardware up to the time.
+         *
+         * @param time The time to playback up to (seconds).
+         * @return True if playback is still running. False if it has completed.
+         */
+        public boolean playback(double time) throws Exception {
+            List<TimeseriesStream.DataPoint> points = reader.readUntil(time);
+            for (TimeseriesStream.DataPoint p : points) {
+                HardwareDevice device = hardware.get(p.varname);
+                if (device != null) {
+                    if (device instanceof DcMotor) {
                        /* if (p.varname.equals("motor2") || p.varname.equals("motor4")) {
                             ((DcMotor)device).setPower(-p.value);
                         } else {
                             ((DcMotor)device).setPower(p.value);
                          }*/
-            ((DcMotor)device).setPower(p.value);
-          }
-          if (device instanceof Servo) {
-            ((Servo)device).setPosition(p.value);
-          }
+                        ((DcMotor) device).setPower(p.value);
+                    }
+                    if (device instanceof Servo) {
+                        ((Servo) device).setPosition(p.value);
+                    }
+                }
+            }
+            return !reader.readDone();
         }
-      }
-      return !reader.readDone();
-    }
 
-    // The hardware to manipulate.
-    private HardwareMap hardware;
-    // The timeseries stream to read.
-    private TimeseriesStream.Reader reader;
-  }
+        // The hardware to manipulate.
+        private HardwareMap hardware;
+        // The timeseries stream to read.
+        private TimeseriesStream.Reader reader;
+    }
 }
