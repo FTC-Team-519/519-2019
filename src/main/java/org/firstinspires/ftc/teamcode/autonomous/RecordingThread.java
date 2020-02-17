185f5ee0c3d9b6155cc945b1059388ffee111609
Nobody <nobody@team116.org>
2020-02-17 14:48:57 -0500

    Auto formatted

--- src/main/java/org/firstinspires/ftc/teamcode/autonomous/RecordingThread.java
+++ src/main/java/org/firstinspires/ftc/teamcode/autonomous/RecordingThread.java
@@ -10,6 +10,7 @@ public class RecordingThread extends Thread {
     private HardwareMap map;
     private BlackBox.Recorder recorder;
     private ElapsedTime recordTimer;
+
     public RecordingThread(BlackBox.Recorder recorder, HardwareMap map) {
         this.map = map;
         this.recorder = recorder;
